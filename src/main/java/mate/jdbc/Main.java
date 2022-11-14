7e949ae5ab50da1533a7fa1c895abb70abd7234a
MuzykaVP <muzykavp.work@gmail.com>
2022-11-14 14:33:24 +0200

    Finish

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,33 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer manufacturer = new Manufacturer("Mazda", "Japan");
+        manufacturerService.create(manufacturer);
+        manufacturerService.getAll().forEach(System.out::println);
+        Driver driver = new Driver(null, "Gordon", "F13232221328");
+        Driver driver1 = new Driver(null, "Max", "D6663232299");
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        driver = driverService.create(driver);
+        driver1 = driverService.create(driver1);
+        driverService.getAll().forEach(System.out::println);
+        System.out.println("---------------------------------------");
+        Driver driver2 = new Driver(driver.getId(), "Shlepa", "F13232221328");
+        driverService.update(driver2);
+        driverService.getAll().forEach(System.out::println);
+        System.out.println("---------------------------------------");
+        driverService.delete(driver1.getId());
+        driverService.getAll().forEach(System.out::println);
+
+
+
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,16 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+
+CREATE TABLE `taxi_service`.`drivers` (
+                                          `id` BIGINT(255) NOT NULL AUTO_INCREMENT,
+                                          `name` VARCHAR(50) NOT NULL,
+                                          `license_number` VARCHAR(50) NOT NULL,
+                                          `is_deleted` TINYINT NULL DEFAULT 0,
+                                          PRIMARY KEY (`id`),
+                                          UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8;
+
+
